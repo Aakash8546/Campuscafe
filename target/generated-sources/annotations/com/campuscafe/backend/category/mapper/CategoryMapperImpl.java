e5ea56acb6b680cefc39cfa26e3d3cb85b1179e3
Aakash Srivastava <aakashsrivastava2151@gmail.com>
2026-06-26 12:19:22 +0530

    added discounts

--- target/generated-sources/annotations/com/campuscafe/backend/category/mapper/CategoryMapperImpl.java
+++ target/generated-sources/annotations/com/campuscafe/backend/category/mapper/CategoryMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-26T10:36:15+0530",
+    date = "2026-06-26T11:45:07+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
