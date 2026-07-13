7cebee3dc0cfd6198d7e42014d3b7522c979abf2
Aakash Srivastava <aakashsrivastava2151@gmail.com>
2026-07-13 17:15:45 +0530

    docs: add javadocs to MerchantNotFoundException

--- src/main/java/com/campuscafe/backend/exception/MerchantNotFoundException.java
+++ src/main/java/com/campuscafe/backend/exception/MerchantNotFoundException.java
@@ -1,5 +1,8 @@
 package com.campuscafe.backend.exception;
 
+/**
+ * Exception thrown when a requested merchant account does not exist in the database.
+ */
 public class MerchantNotFoundException extends RuntimeException {
     public MerchantNotFoundException(String message) {
         super(message);
