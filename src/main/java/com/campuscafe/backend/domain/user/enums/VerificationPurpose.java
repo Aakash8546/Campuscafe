dffacf9a724e87c07108d568cf2a3c88747b8b90
Aakash Srivastava <aakashsrivastava2151@gmail.com>
2026-07-13 17:16:06 +0530

    docs: add javadocs to VerificationPurpose enum

--- src/main/java/com/campuscafe/backend/domain/user/enums/VerificationPurpose.java
+++ src/main/java/com/campuscafe/backend/domain/user/enums/VerificationPurpose.java
@@ -1,6 +1,16 @@
 package com.campuscafe.backend.domain.user.enums;
 
+/**
+ * Purpose description of a generated OTP verification token.
+ */
 public enum VerificationPurpose {
+    /**
+     * Sent to user email to verify ownership of the account email address.
+     */
     EMAIL_VERIFICATION,
+
+    /**
+     * Sent to user email to authorize password reset requests.
+     */
     PASSWORD_RESET
 }
