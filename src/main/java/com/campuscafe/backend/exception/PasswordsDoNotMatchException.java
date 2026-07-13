48e466518d9bd5b8658909a1e9537ed5837b96f7
Aakash Srivastava <aakashsrivastava2151@gmail.com>
2026-07-13 17:15:27 +0530

    docs: add javadocs to PasswordsDoNotMatchException

--- src/main/java/com/campuscafe/backend/exception/PasswordsDoNotMatchException.java
+++ src/main/java/com/campuscafe/backend/exception/PasswordsDoNotMatchException.java
@@ -1,5 +1,8 @@
 package com.campuscafe.backend.exception;
 
+/**
+ * Exception thrown when the password and confirm password fields do not match during signup.
+ */
 public class PasswordsDoNotMatchException extends RuntimeException {
     public PasswordsDoNotMatchException(String message) {
         super(message);
